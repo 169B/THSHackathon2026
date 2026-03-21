8d1651910af31bd45257ddf0fa5033dc9bb6e43c
Theo Berkman <theoberk@haverford.org>
2026-03-20 21:29:22 -0400

    hi jayden

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        // hi
         <link rel="icon" href="/appwrite.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
